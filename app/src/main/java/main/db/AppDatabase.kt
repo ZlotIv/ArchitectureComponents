2abed494fa467bc474268c3d538867472740934b
MOBILEDIMENSION\izlotnikov <izlotnikov@mobiledimension.ru>
2018-06-14 10:54:00 +0300

    fix db(2)

--- app/src/main/java/main/db/AppDatabase.kt
+++ app/src/main/java/main/db/AppDatabase.kt
@@ -7,6 +7,7 @@ import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
 import android.content.ContentValues
 import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import main.App
 import main.db.dao.ContactDao
@@ -17,27 +18,18 @@ abstract class AppDatabase: RoomDatabase() {
     abstract fun contactsDao(): ContactDao
 
     companion object {
-        val database = Room.databaseBuilder(App.appContext, AppDatabase::class.java, "database")
-                .allowMainThreadQueries()
-                .addCallback(object : RoomDatabase.Callback() {
-                    override fun onCreate(db: SupportSQLiteDatabase) {
-                        super.onCreate(db)
-                        Completable.create {
-                            listArray().mapIndexed { index, contact ->
-                                val contentValues = ContentValues()
-                                contentValues.put("first_name", "${contact.firstName}$index")
-                                contentValues.put("last_name", "${contact.lastName}$index")
-                                contentValues.put("age", contact.age + index)
-                                contentValues.put("post", contact.post)
-                                contentValues.put("phone_number", contact.phoneNumber)
-                                db.insert("Contact", OnConflictStrategy.IGNORE, contentValues)
-                            }
-                        }
-                                .subscribeOn(Schedulers.computation())
-                                .subscribe()
-                    }
-                })
-                .build()
+        val database by lazy {
+            val room = Room.databaseBuilder(App.appContext, AppDatabase::class.java, "database").build()
+            Completable.create {
+                listArray().mapIndexed { index, contact ->
+                    room.contactsDao().insertAll(contact)
+                }
+            }
+                    .subscribeOn(Schedulers.computation())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe()
+            return@lazy room
+        }
         private fun listArray(): Array<Contact> {
             return Array(24, { i ->
                 Contact(firstName = "User FirstName$i", lastName = "User LastName$i", age = i, phoneNumber = 1234567890, post = "programmer")
