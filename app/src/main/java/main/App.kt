7cde460c4edd2ed340b65862b65278dfd3e80abe
Ivan Zlotnikov <zlat200895@gmail.com>
2018-06-14 10:26:31 +0300

    fix db

--- app/src/main/java/main/App.kt
+++ app/src/main/java/main/App.kt
@@ -18,38 +18,11 @@ class App: Application() {
 
     companion object {
         lateinit var appContext: Context
-        lateinit var dataBase: AppDatabase
     }
 
     override fun onCreate() {
         super.onCreate()
         appContext = this
         Stetho.initializeWithDefaults(this)
-        dataBase = Room.databaseBuilder(this, AppDatabase::class.java, "database")
-                .addCallback(object : RoomDatabase.Callback() {
-                    override fun onCreate(db: SupportSQLiteDatabase) {
-                        super.onCreate(db)
-                        Completable.create {
-                            listArray().mapIndexed { index, contact ->
-                                val contentValues = ContentValues()
-                                contentValues.put("first_name", "${contact.firstName}$index")
-                                contentValues.put("last_name", "${contact.lastName}$index")
-                                contentValues.put("age", contact.age + index)
-                                contentValues.put("post", contact.post)
-                                contentValues.put("phone_number", contact.phoneNumber)
-                                db.insert("Contact", OnConflictStrategy.IGNORE, contentValues)
-                            }
-                        }
-                                .subscribeOn(Schedulers.computation())
-                                .subscribe()
-                    }
-                })
-                .build()
-    }
-
-    fun listArray(): Array<Contact> {
-        return Array(24, { i ->
-            Contact(firstName = "User FirstName$i", lastName = "User LastName$i", age = i, phoneNumber = 1234567890, post = "programmer")
-        })
     }
 }
--- app/src/main/java/main/model/ContactRepository.kt
+++ app/src/main/java/main/model/ContactRepository.kt
@@ -2,12 +2,13 @@ package main.model
 
 import android.arch.paging.DataSource
 import main.App
+import main.db.AppDatabase
 import main.db.dao.ContactDao
 import main.db.entity.Contact
 
 class ContactRepository {
 
-    private val dao: ContactDao by lazy { App.dataBase.contactsDao() }
+    private val dao: ContactDao by lazy { AppDatabase.database.contactsDao() }
 
     fun getData(): DataSource.Factory<Int, Contact> = dao.getAll()
 }
